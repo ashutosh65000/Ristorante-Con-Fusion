940f43abec4c461b4b15d6d7569b1b16d6fba966
Ashutosh Srivastava <ashutosh65000@outlook.com>
2020-08-18 21:56:34 +0530

    Single Page Applications Part1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,32 +1,47 @@
 import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Header from './HeaderComponent';
+import Contact from './ContactComponent';
 import Menu from './MenuComponent'
 import DishDetail from './DishdetailComponent'
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            dishes: DISHES
+            dishes: DISHES,
+            comments: COMMENTS,
+            leaders: LEADERS,
+            promotions: PROMOTIONS
         }
     }
 
     render() {
-        const HomePage= ()=>{
-            return(
-                <Home />
+        const HomePage = () => {
+            return (
+                <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                    promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                    leader={this.state.leaders.filter((lead) => lead.featured)[0]}
+                />
             )
         }
         return (
             <div>
                 <Header />
+                {/* Switch is used to group mutliple routes together as a group, it also displays the first matching route in the series of multiple routes. */}
                 <Switch>
+                    {/* If you don't want to pass any props to the component then use this way. */}
                     <Route path="/home" component={HomePage} />
+                    {/* If props are required then use the ES6 arrow function to pass the props. */}
                     <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+                    <Route exact path="/contactus" component={Contact} />
+                    {/* Redirect routes to the route path described if no matching route is found */}
                     <Redirect to="/home" />
                 </Switch>
                 <Footer />
